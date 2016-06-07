0be37266cacadd095d8a98036f853bef163356e8
Jamil Atta Junior <atta.jamil@gmail.com>
2016-06-07 17:00:12 -0300

    Versão inicial do processamento AM

---
+++ config.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import os
+import datetime
+from datetime import timedelta
+
+FROM_DATE = (datetime.datetime.now()-timedelta(60)).isoformat()[:10]
+
+XML_CSS = "/static/css/style_article_html.css"
+
+ARTICLE_META_URL = 'http://articlemeta.scielo.org/'
+
+APP_URL = 'http://homolog.opac.scielo.org'
+
+MONGODB_DBNAME = os.environ.get('OPAC_MONGO_DB_DBNAME', 'opac')
+MONGODB_HOST = os.environ.get('OPAC_MONGO_PORT_27017_TCP_ADDR', 'localhost')
+MONGODB_PORT = os.environ.get('OPAC_MONGO_PORT_27017_TCP_PORT', 27017)
+
+MONGODB_SETTINGS = {
+    'db': MONGODB_DBNAME,
+    'host': MONGODB_HOST,
+    'port': int(MONGODB_PORT),
+}
