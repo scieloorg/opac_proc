fa7b33ac70a70d808ffd5bd9cd37b8a38664a779
Jamil Atta Junior <atta.jamil@gmail.com>
2016-08-03 12:17:34 -0300

    Artualização .sh e atualização no Xylose

--- run_process.sh
+++ run_process.sh
@@ -1,6 +1,6 @@
 docker run -v /tmp/opac_proc_logs/:/app/logs/ \
     -e OPAC_PROC_COLLECTION="sss" \
-    -e OPAC_PROC_MONGODB_HOST="172.17.0.2" \
-    -e OPAC_PROC_MONGODB_NAME="opac_test" \
+    -e OPAC_PROC_MONGODB_HOST="192.168.169.196" \
+    -e OPAC_PROC_MONGODB_NAME="opac" \
     -e OPAC_PROC_LOG_LEVEL="DEBUG" \
-    opac_proc
+    scieloorg/opac_proc
