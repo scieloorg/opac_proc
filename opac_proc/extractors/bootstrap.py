c4d4434b4bd11bc879afc5249e4d632ab6b48bd7
Juan Funez <juan.funez@gmail.com>
2016-08-13 15:00:29 -0300

    melhorias

--- opac_proc/extractors/bootstrap.py
+++ opac_proc/extractors/bootstrap.py
@@ -9,8 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 def run(collection_acronym):
-    collection_id = extraction.CExtactor(collection_acronym)
-    transformed_collection = transform_collection(collection_data)
+    coll_extractor = extraction.CExtactor(collection_acronym)
+    coll_extractor.run()
+    col = coll_extractor.get_extracted_raw_data()[0]
+    print(col)
+    import pdb; pdb.set_trace()
+    # collection.save()
+    # transformed_collection = transform_collection(collection_data)
 
 
 if __name__ == '__main__':
--- opac_proc/extractors/collections/extraction.py
+++ opac_proc/extractors/collections/extraction.py
@@ -4,6 +4,7 @@ from opac_proc.extractors.source_clients.thrift import am_clients
 from opac_proc.datastore.mongodb_connector import get_db_connection
 from opac_proc.datastore.models.collection import Collection
 import logging
+import config
 
 
 logger = logging.getLogger(__name__)
@@ -13,22 +14,51 @@ class CExtactor(object):
     """docstring for CExtactor"""
     acronym = None
     db = None
-    raw_data = None
+    raw_data = []
+    articlemeta = None
+
+    extract_attributes = [
+        # atributos a serem extraidos
+        'code',
+        'name',
+        'acronym',
+    ]
 
     def __init__(self, acronym):
         self.acronym = acronym
         self.db = get_db_connection()
+        self.articlemeta = am_clients.ArticleMeta(
+            config.ARTICLE_META_THRIFT_DOMAIN,
+            config.ARTICLE_META_THRIFT_PORT)
 
-    def run(self):
-        # Collection
-        for col in articlemeta.collections():
+    def extract(self):
+        """
+        Conecta com a fonte (AM) e extrai todos os dados (coleção).
+        -> método: get_raw_data para obter os resultados do extract
+        -> método: save() para salvar
+        -> método: to_python() para retornar os dados coletados como objetos pytho (dict/lists)
+        """
+        for col in self.articlemeta.collections():
             if col.acronym == self.acronym:
                 logger.info(u"Adicionado a coleção: %s" % self.acronym)
-                self.raw_data = col
+                self.raw_data.append(col)
                 break
 
+    def filter_raw_data(self):
+        return 'TODO!'
+
+    @property
+    def get_raw_data(self):
+        """
+        Retorna a todos os dados coletados de forma crúa (objetos thrift)
+        """
+        return self.raw_data
+
     def save(self):
-        """" docstring aqui """
+        """"
+        Salva os dados coletados no datastore (mongo)
+        """
+
         try:
             if self.raw_data:
                 c = Collection()
@@ -38,3 +68,5 @@ class CExtactor(object):
                 logger.error("Não foi possível salvar a Coleção (acronym: %s). A informação é vazía" % self.acronym)
         except Exception, e:
             logger.error("Não foi possível salvar a Coleção (acronym: %s). %s" % (self.acronym, str(e)))
+            raise e
+
