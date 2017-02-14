486af80a174e8ec9ba21d0baafe4880f9f9536f8
Bruno Melo <brunousml@gmail.com>
2017-02-14 18:27:25 -0200

    Adiciona tarefa de importação de press releases

--- opac_proc/extractors/ex_press_releases.py
+++ opac_proc/extractors/ex_press_releases.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import json
+
 import feedparser
 
 from datetime import datetime
@@ -51,7 +53,7 @@ class PressReleaseExtractor(BaseExtractor):
         # logger.info(u'Inicia PressReleasesExtractor.extract(%s) %s' % (self.acronym, datetime.now()))
 
         raw_journal = self.get_items_from_feed()
-        self._raw_data = raw_journal
+        self._raw_data = dict(raw_journal)
 
         if not self._raw_data:
             return False
@@ -61,3 +63,55 @@ class PressReleaseExtractor(BaseExtractor):
 
         logger.info(u"Fim PressReleasesExtractor.extract(%s) %s" % (
             self.acronym, datetime.now()))
+
+    def save(self):
+        """
+        Salva os dados coletados no datastore (mongo)
+        """
+        logger.debug(u"Inciando metodo save()")
+
+        if self.metadata['is_locked']:
+            msg = u"atributos metadata['is_locked'] indica que o processamento não finalizou corretamente."
+            logger.error(msg)
+            raise Exception(msg)
+        elif self.extract_model_class is None or self.extract_model_name is None:
+            msg = u"atributos extract_model_class ou extract_model_name não forma definidos na subclasse"
+            logger.error(msg)
+            raise Exception(msg)
+        elif self.metadata['process_start_at'] is None:
+            msg = u"não foi definida o timestamp de inicio, você definiu/invocou o metodo: extract() na subclasse?"
+            logger.error(msg)
+            raise Exception(msg)
+        # elif not self._raw_data:
+        #     msg = u"os dados coletados estão vazios, você definiu/invocou o metodo: extract() na subclasse?"
+        #     logger.error(msg)
+        #     raise Exception(msg)
+        elif not isinstance(self._raw_data, dict):
+            msg = u"os dados extraidos, não são do tipo esperado: dict()"
+            logger.error(msg)
+            raise Exception(msg)
+        else:
+            # atualizamos as datas no self.metadata
+            self.metadata['must_reprocess'] = False
+            self._raw_data.update(**self.metadata)
+            self.extract_model_instance = self.get_extract_model_instance()
+            # salvamos no mongo
+            try:
+                if self.extract_model_instance:
+                    logger.debug(u"extract_model_instance encontrado. Atualizando!")
+                    self.extract_model_instance.modify(**self._raw_data)
+                else:
+                    logger.debug(u"extract_model_instance NÃO encontrado. Criando novo!")
+                    self.extract_model_instance = self.extract_model_class(**self._raw_data)
+                    self.extract_model_instance.save()
+            except Exception, e:
+                msg = u"Não foi possível salvar %s. Exceção: %s" % (
+                    self.extract_model_name, e)
+                logger.error(msg)
+                raise e
+            else:
+                logger.debug(u"Reload de extract_model_instance")
+                self.extract_model_instance.reload()
+                logger.debug(u"Fim metodo save(), retornamos uuid: %s" % self.extract_model_instance.uuid)
+                return self.extract_model_instance
+
