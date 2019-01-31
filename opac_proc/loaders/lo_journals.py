1143cb0c4030f7591a0dbe5e369817054d7feee9
Cesar Augusto <cesar.bruschetta@scielo.org>
2019-01-31 14:35:45 -0200

    Atualização do opac-schema para adicionar o novo campo 'new_title' no Journal

--- opac_proc/loaders/lo_journals.py
+++ opac_proc/loaders/lo_journals.py
@@ -52,6 +52,7 @@ class JournalLoader(BaseLoader):
         'social_networks',
         'title',
         'title_iso',
+        'next_title',
         'short_title',
         'title_slug',
         'created',
